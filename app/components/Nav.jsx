cc9781668b9205e46ce48b0e1ab073e427348b76
Jeff Miller <jeffery.f@gmail.com>
2016-10-10 22:21:06 -0500

    Use arrow functions where possible

--- app/components/Nav.jsx
+++ app/components/Nav.jsx
@@ -1,17 +1,15 @@
 var React = require('react');
 var {Link, IndexLink} = require('react-router');
 
-var Nav = React.createClass({
-  render: function () {
-    return (
-      <div>
-        <h2>Nav Component</h2>
-        <IndexLink to="/" activeClassName="active">Get Weather</IndexLink>
-        <Link to="/about" activeClassName="active">About</Link>
-        <Link to="/examples" activeClassName="active">Examples</Link>
-      </div>
-    );
-  }
-});
+var Nav = (props) => {
+  return (
+    <div>
+      <h2>Nav Component</h2>
+      <IndexLink to="/" activeClassName="active">Get Weather</IndexLink>
+      <Link to="/about" activeClassName="active">About</Link>
+      <Link to="/examples" activeClassName="active">Examples</Link>
+    </div>
+  );
+}
 
 module.exports = Nav;
--- app/components/WeatherMessage.jsx
+++ app/components/WeatherMessage.jsx
@@ -1,14 +1,11 @@
 var React = require('react');
 
-var WeatherMessage = React.createClass({
-  render: function () {
-    var {isLoading, temp, location} = this.props;
-    return (
-      <div>
-        <p>It is {temp} in {location}</p>
-      </div>
-    );
-  }
-});
+var WeatherMessage = ({temp, location}) => {
+  return (
+    <div>
+      <p>It is {temp} in {location}</p>
+    </div>
+  );
+};
 
 module.exports = WeatherMessage;
